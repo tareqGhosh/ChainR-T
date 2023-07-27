c81d5a83a004ed3195bf25beabcb07213e0aa15d
TareqAbughosh <30448064+TareqAbughosh@users.noreply.github.com>
2023-07-27 14:14:22 +0300

    removed pw from being selected

--- models/user.js
+++ models/user.js
@@ -1,9 +1,16 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
+const PROTECTED_ATTRIBUTES = ["password"];
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
+    toJSON() {
+      // hide protected fields
+      let attributes = Object.assign({}, this.get());
+      for (let a of PROTECTED_ATTRIBUTES) {
+        delete attributes[a];
+      }
+      return attributes;
+    }
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,15 +20,23 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  User.init({
-    first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
-    email: {type: DataTypes.STRING, unique: true},
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-    tableName: 'users'
-  });
+  User.init(
+    {
+      first_name: DataTypes.STRING,
+      last_name: DataTypes.STRING,
+      email: { type: DataTypes.STRING, unique: true },
+      password: {
+        type: DataTypes.STRING,
+        get() {
+          return undefined;
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+      tableName: "users",
+    }
+  );
   return User;
 };
